0fd136e352d67fe7075e6f20600512eabef426bf
Philliph Drummond <oilsinwater@gmail.com>
2022-06-12 01:36:18 -0700

    Bug: netlify failing production build.

--- gatsby-config.js
+++ gatsby-config.js
@@ -200,5 +200,6 @@ module.exports = {
         `gatsby-plugin-catch-links`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-offline`,
+        `gatsby-plugin-netlify-cms`,
     ],
 };
